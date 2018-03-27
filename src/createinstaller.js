57ce83f09908d26b2951c584dd568e00ad6cccb4
diego-galocha <diego.galocha@ezzing.com>
2018-03-27 23:58:05 +0200

    Added installer

---
+++ src/createinstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig () {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'release-builds')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'Avi-checker-win32-ia32/'),
+    authors: 'Diego Galocha',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'windows-installer'),
+    exe: 'avi-checker-app.exe',
+    setupExe: 'AviCheckerAppInstaller.exe',
+    setupIcon: path.join(rootPath, 'assets', 'icons', 'main-icon.ico')
+  })
+}
