bf46e526f81ad14ec943eb6605396d78ed9faa04
diego-galocha <diego.galocha@ezzing.com>
2018-03-28 00:28:13 +0200

    Changed paths to create installer

--- src/createinstaller.js
+++ src/createinstaller.js
@@ -11,15 +11,15 @@ getInstallerConfig()
 function getInstallerConfig () {
   console.log('creating windows installer')
   const rootPath = path.join('./')
-  const outPath = path.join(rootPath, 'release-builds')
+  const outPath = path.join(rootPath, 'out')
 
   return Promise.resolve({
-    appDirectory: path.join(outPath, 'Avi-checker-win32-ia32/'),
+    appDirectory: path.join(outPath, 'avi-checker-win32-ia32/'),
     authors: 'Diego Galocha',
     noMsi: true,
     outputDirectory: path.join(outPath, 'windows-installer'),
-    exe: 'avi-checker-app.exe',
+    exe: 'avi-checker.exe',
     setupExe: 'AviCheckerAppInstaller.exe',
-    setupIcon: path.join(__dirname, 'assets', 'icon', 'main-icon.png')
+    setupIcon: path.join(rootPath, 'src', 'assets', 'icons', 'main-icon.ico')
   })
 }
